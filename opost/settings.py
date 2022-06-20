52c62e00357da382eed0285689e3d8ba3ce1e6dd
Owen Smith <ods94065@gmail.com>
2022-06-20 12:03:44 -0700

    opost: reshuffle settings.py
    I converted settings.py by simply regenerating a fresh settings.py in
    Django 4.x and porting all our settings to it.
    
    This reorganizes the old settings module to match the approximate order
    of Django 4.x's settings.py, so that the changes to settings.py in the
    upgrade will be more obvious.

--- opost/settings.py
+++ opost/settings.py
@@ -13,33 +13,68 @@ env = environ.Env(
     ALLOWED_HOSTS=([str], []),
 )
 
+SECRET_KEY = env("SECRET_KEY")
 DEBUG = env("DEBUG")
 TEMPLATE_DEBUG = DEBUG
-
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-    ("Opost Admin", "ods94043@yahoo.com"),
-)
-
-MANAGERS = ADMINS
-
-DATABASES = {"default": env.db_url()}
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env("ALLOWED_HOSTS")
+
+INSTALLED_APPS = (
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.admin",
+    "django.contrib.admindocs",
+    "django_extensions",
+    "postapi",
+    "postweb",
+    "rest_framework",
+)
+
+MIDDLEWARE_CLASSES = (
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = "opost.urls"
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    "django.template.loaders.filesystem.Loader",
+    "django.template.loaders.app_directories.Loader",
+    #     'django.template.loaders.eggs.Loader',
+)
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+# Python dotted path to the WSGI application used by Django's runserver.
+WSGI_APPLICATION = "opost.wsgi.application"
+
+DATABASES = {"default": env.db_url()}
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = env("LANGUAGE_CODE")
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
 TIME_ZONE = env("TIME_ZONE")
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = env("LANGUAGE_CODE")
-
-SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
@@ -83,52 +118,6 @@ STATICFILES_FINDERS = (
     #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = env("SECRET_KEY")
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    "django.template.loaders.filesystem.Loader",
-    "django.template.loaders.app_directories.Loader",
-    #     'django.template.loaders.eggs.Loader',
-)
-
-MIDDLEWARE_CLASSES = (
-    "django.middleware.common.CommonMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-ROOT_URLCONF = "opost.urls"
-
-# Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = "opost.wsgi.application"
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
-INSTALLED_APPS = (
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.sites",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "django.contrib.admin",
-    "django.contrib.admindocs",
-    "django_extensions",
-    "postapi",
-    "postweb",
-    "rest_framework",
-)
-
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
@@ -168,6 +157,18 @@ LOGGING = {
     },
 }
 
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+    ("Opost Admin", "ods94043@yahoo.com"),
+)
+
+MANAGERS = ADMINS
+
+LOGIN_REDIRECT_URL = "/"
+LOGIN_URL = "/web/login"
+
+SITE_ID = 1
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
@@ -183,8 +184,6 @@ REST_FRAMEWORK = {
     ],
 }
 
-LOGIN_REDIRECT_URL = "/"
-LOGIN_URL = "/web/login"
 SERVICES = {
     "postapi": {
         "endpoint": "http://localhost:5100/postapi/",
